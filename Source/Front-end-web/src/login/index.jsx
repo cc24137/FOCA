52635cb2e448fb189ad0401c463668e3802885fb
Eduardo Tribst <eduardo.tribst@gmail.com>
2026-04-12 11:37:04 -0300

    componente de titulo lateral

--- Source/Front-end-web/src/login/index.jsx
+++ Source/Front-end-web/src/login/index.jsx
@@ -1,20 +1,15 @@
 import { useNavigate } from 'react-router-dom';
 import SeletorTipo from "../components/selecionar-tipo";
 import { useState } from "react";
+import "./login.css";
+import TituloLateral from '../components/titulo-lateral';
   
 export default function Login(){
     const [tipoSelecionado, setTipoSelecionado] = useState("professor");
 
     return (
         <div>
-            <h1>Login</h1>
-
-            <SeletorTipo
-            opcao={tipoSelecionado}
-            aoSelecionar={setTipoSelecionado}
-            />
-
-            <h1>{tipoSelecionado}</h1>
+            <TituloLateral />
         </div>
         
     )

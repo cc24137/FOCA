c6e232561cb5af2ebff427ae9d5ac61eaf53b400
EduardoTribst <eduardo.tribst@gmail.com>
2026-04-15 12:17:08 -0300

    tela codigo com algum erro bizarro

--- Source/Front-end-web/src/codigo-email/index.jsx
+++ Source/Front-end-web/src/codigo-email/index.jsx
@@ -1,7 +1,55 @@
 import { useNavigate } from 'react-router-dom';
+import TituloLateral from '../components/titulo-lateral';
+import HomeIcon from "../assets/home.svg?react";
+import './codigo-email.css';
+import OtpInput from 'react-otp-input';
+import { useState } from 'react';
 
-export default function CodigoEmail(){
-    return (
-        <h1>Codigo Email</h1>
-    )
-}
+// export default function CodigoEmail(){
+//     const [code, setCode] = useState('');
+
+//     return (
+//         <div className='codigo-email-body'>
+//             <TituloLateral />
+
+//             <div className='codigo-email-right'>
+
+            
+//                 <button
+//                     className="codigo-email-button"
+//                     onClick={() => goTo("/")}
+//                 >
+//                     <div className="codigo-email-content-button">
+//                         <HomeIcon className="home-icon" />
+//                         <span className='codigo-email-button-text'>Início</span>
+//                     </div>
+//                 </button>
+            
+//                 <div className='codigo-email-center'>
+//                     <div className='codigo-email-box'>
+//                         <p>Quase lá!</p>
+//                         <p>Insira o código de verificação enviado para seu email para finalizar a recuperação da sua conta.</p>
+//                     </div>
+                
+//                     <OtpInput
+//                         value={code}
+//                         onChange={setCode}
+//                         numInputs={6}
+//                         renderInput={(props) => <input {...props} />}
+//                     />
+//                 <div />
+
+                
+//             </div>
+
+//             <div className='codigo-email-bottom'>
+//                 <button 
+//                     className='codigo-email-submit-button'
+//                     onClick={() => formSubmit()}
+//                 >
+//                     Confirmar Código
+//                 </button>
+//             </div>
+//         </div>
+//     );
+// }

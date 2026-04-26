0281b8f85f98916b07131c6a6f4540e8b621b0c0
Júlio Pacheco Stein <juliopstein@gmail.com>
2026-04-26 19:25:45 -0300

    Mudanças no banco de dados

--- Source/API/config/create_tables_locally.sql
+++ Source/API/config/create_tables_locally.sql
@@ -1,101 +1,105 @@
-
 CREATE TABLE foca.Professor 
 (
- id INT PRIMARY KEY identity(1, 1),  
- email VARCHAR(100),  
- nome VARCHAR(100) NOT NULL,  
- senha_hash VARCHAR(255) NOT NULL,  
- emailVerificado bit default 0,
- CHECK (email LIKE '%@%'),
- UNIQUE (email)
-);
-
-CREATE TABLE foca.Turma
-(
- id INT PRIMARY KEY identity(1, 1),  
- nome VARCHAR(100),  
- id_instituicao INT,  
- numero_alunos INT,  
- ano TINYINT,
-);
-
-CREATE TABLE foca.Disciplina
-(
- id INT PRIMARY KEY identity(1, 1),  
- nome VARCHAR(100) NOT NULL,  
- id_instituicao INT,  
-);
-
-CREATE TABLE foca.Relatorio
-(
- id_aula INT,  
- id INT PRIMARY KEY identity(1, 1),  
- media_atencao_total FLOAT,  
- data_processamento DATE,  
-);
-
-CREATE TABLE foca.Aula
-(
- id INT PRIMARY KEY identity(1, 1),  
- data DATE NOT NULL,  
- conteudo VARCHAR(350),  
- id_turma_disciplina_professor INT
+    id INT PRIMARY KEY IDENTITY(1, 1),  
+    email VARCHAR(100),  
+    nome VARCHAR(100) NOT NULL,  
+    senha_hash VARCHAR(255) NOT NULL,  
+    emailVerificado BIT DEFAULT 0,
+    CHECK (email LIKE '%@%'),
+    UNIQUE (email)
 );
 
 CREATE TABLE foca.Instituicao
 (
- id INT PRIMARY KEY identity(1, 1),  
- nome VARCHAR(100) NOT NULL,  
- senha_hash VARCHAR(255) NOT NULL,  
- email VARCHAR(100),  
- emailVerificado bit default 0,
- UNIQUE (nome),
- unique (email)
+    id INT PRIMARY KEY IDENTITY(1, 1),  
+    nome VARCHAR(100) NOT NULL,  
+    senha_hash VARCHAR(255) NOT NULL,  
+    email VARCHAR(100),  
+    emailVerificado BIT DEFAULT 0,
+    UNIQUE (nome),
+    UNIQUE (email)
 );
 
-CREATE TABLE foca.Instituicao_Professor
+CREATE TABLE foca.Turma
 (
- id_instituicao INT,  
- id_professor INT,  
+    id INT PRIMARY KEY IDENTITY(1, 1),  
+    nome VARCHAR(100),  
+    id_instituicao INT,  
+    numero_alunos INT,  
+    ano TINYINT
 );
 
-CREATE TABLE foca.MomentoDestaque
+CREATE TABLE foca.Disciplina
 (
- id INT PRIMARY KEY identity(1, 1),  
- percentual_atencao FLOAT,  
- id_relatorio INT,  
- tempo_inicio TIME,  
- tempo_fim TIME,  
+    id INT PRIMARY KEY IDENTITY(1, 1),  
+    nome VARCHAR(100) NOT NULL,  
+    id_instituicao INT
 );
 
 CREATE TABLE foca.Turma_Disciplina_Professor
 (
- id_disciplina INT,  
- id_turma INT,  
- id_professor INT,  
- id INT PRIMARY KEY identity(1, 1),  
+    id INT PRIMARY KEY IDENTITY(1, 1),  
+    id_disciplina INT,  
+    id_turma INT,  
+    id_professor INT
+);
+
+CREATE TABLE foca.Instituicao_Professor
+(
+    id_instituicao INT,  
+    id_professor INT
+);
+
+CREATE TABLE foca.Classificacao_Conteudo
+(
+    id INT PRIMARY KEY IDENTITY(1, 1),
+    nome VARCHAR(100) NOT NULL, -- Ex: 'Explicação Teórica', 'Prova', 'Apresentação'
+    descricao VARCHAR(255)
+);
+
+CREATE TABLE foca.Aula
+(
+    id INT PRIMARY KEY IDENTITY(1, 1),  
+    data DATE NOT NULL,  
+    conteudo VARCHAR(350),  
+    id_turma_disciplina_professor INT NOT NULL,
+    id_classificacao_conteudo INT,
+    -- dados do relatorio
+    arquivo_video VARCHAR(255),
+    media_atencao_total FLOAT,  
+    data_processamento DATE
+);
+
+CREATE TABLE foca.Leitura_Atencao
+(
+    id_aula INT NOT NULL,
+    segundo_video INT NOT NULL, -- Ex: 5, 10, 15... (segundos desde o início da aula)
+    indice_atencao DECIMAL(5,2) NOT NULL, -- Ex: 85.50
+    PRIMARY KEY (id_aula, segundo_video) -- Chave primária composta
 );
 
 CREATE TABLE foca.Codigo_Verificacao
 (
- id INT PRIMARY KEY IDENTITY(1, 1),
- o_codigo CHAR(6),
- email VARCHAR(100)
- CreatedAt DATETIME DEFAULT GETDATE(),
- ExpiresAt DATETIME NOT NULL,
+    id INT PRIMARY KEY IDENTITY(1, 1),
+    o_codigo CHAR(6),
+    email VARCHAR(100),
+    CreatedAt DATETIME DEFAULT GETDATE(),
+    ExpiresAt DATETIME NOT NULL
 );
 
+-- chaves estrangeiras
 
 ALTER TABLE foca.Turma ADD FOREIGN KEY(id_instituicao) REFERENCES foca.Instituicao (id);
 ALTER TABLE foca.Disciplina ADD FOREIGN KEY(id_instituicao) REFERENCES foca.Instituicao (id);
-ALTER TABLE foca.Relatorio ADD FOREIGN KEY(id_aula) REFERENCES foca.Aula (id);
-ALTER TABLE foca.Aula ADD FOREIGN KEY(id_turma_disciplina_professor) REFERENCES foca.Turma_Disciplina_Professor (id);
-ALTER TABLE foca.Instituicao_Professor ADD FOREIGN KEY(id_instituicao) REFERENCES foca.Instituicao (id);
-ALTER TABLE foca.Instituicao_Professor ADD FOREIGN KEY(id_professor) REFERENCES foca.Professor (id);
-ALTER TABLE foca.MomentoDestaque ADD FOREIGN KEY(id_relatorio) REFERENCES foca.Relatorio (id);
+
 ALTER TABLE foca.Turma_Disciplina_Professor ADD FOREIGN KEY(id_disciplina) REFERENCES foca.Disciplina (id);
 ALTER TABLE foca.Turma_Disciplina_Professor ADD FOREIGN KEY(id_turma) REFERENCES foca.Turma (id);
 ALTER TABLE foca.Turma_Disciplina_Professor ADD FOREIGN KEY(id_professor) REFERENCES foca.Professor (id);
 
-EXEC sp_rename 'foca.MomentoDestaque', 'Momento_Destaque';
+ALTER TABLE foca.Instituicao_Professor ADD FOREIGN KEY(id_instituicao) REFERENCES foca.Instituicao (id);
+ALTER TABLE foca.Instituicao_Professor ADD FOREIGN KEY(id_professor) REFERENCES foca.Professor (id);
 
+ALTER TABLE foca.Aula ADD FOREIGN KEY(id_turma_disciplina_professor) REFERENCES foca.Turma_Disciplina_Professor (id);
+ALTER TABLE foca.Aula ADD FOREIGN KEY(id_classificacao_conteudo) REFERENCES foca.Classificacao_Conteudo (id);
+
+ALTER TABLE foca.Leitura_Atencao ADD FOREIGN KEY(id_aula) REFERENCES foca.Aula (id);

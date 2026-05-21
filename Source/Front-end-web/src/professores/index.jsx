9901fd33bb2fbaaa0c298f1c0ce9953719d61f77
Rafael Fazion B. Dias <rafafazion9@gmail.com>
2026-05-21 12:11:04 -0300

    integração tela professores não funcionando

--- Source/Front-end-web/src/professores/index.jsx
+++ Source/Front-end-web/src/professores/index.jsx
@@ -2,8 +2,51 @@ import { useNavigate } from 'react-router-dom';
 import Header from '../components/header';
 import './professores.css';
 import { useState } from 'react';
+import api from "../services/api";
 
 export default function Professores(){
+    
+    let professores = [];
+
+    function putDataInProfessoresArray(prof){
+        professores.forEach((e)=>{
+            if (e.id === prof.id){
+                e.turmas.push(prof.turma);
+                e.disciplinas.push(prof.disciplina);
+            }
+            else{
+                professores.push({
+                    nome: prof.nome, 
+                    email: prof.email,
+                    turmas: [],
+                    disciplinas: [],
+                    mediaAtencao: prof.media_atencao
+                });
+            }
+        });
+    }
+
+    async function loadData(){
+        console.log("Loading data from api to professores");
+        try{
+            const response = await api.get("/professor/infosPorInstituicao");
+            const data = response.data;
+            data.forEach(e => {
+                putDataInProfessoresArray(e);
+            });
+        } 
+        catch(error) {
+            if (error.response){
+                console.log("Erro na API: " + error.response.data.message);
+                alert("Ocorreu um erro inesperado. Tente novamente mais tarde.");
+            }
+            else{
+                alert("Erro de conexão com o servidor.");
+            }
+        }
+    }
+
+    /*
     const professores = [
         { nome: "Ana Paula Souza", email: "ana.souza@escola.com", turmas: ["9A", "9B", "8A"], mediaAtencao: 87.4 },
         { nome: "Carlos Eduardo Lima", email: "carlos.lima@escola.com", turmas: ["7A", "7B"], mediaAtencao: 72.1 },
@@ -13,6 +56,7 @@ export default function Professores(){
         { nome: "Marcos Vinicius Rocha", email: "marcos.rocha@escola.com", turmas: ["6A", "7A"], mediaAtencao: 78.5 },
         { nome: "Patricia Almeida", email: "patricia.almeida@escola.com", turmas: ["8A", "8B", "8C"], mediaAtencao: 95.2 },
     ];
+    */
 
     const [selectedProfessor, setSelectedProfessor] = useState(null);
 

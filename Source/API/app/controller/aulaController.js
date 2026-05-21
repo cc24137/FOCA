0fb31b44e2d574e6f5f911053921b0afcd37a415
Júlio Pacheco Stein <juliopstein@gmail.com>
2026-05-21 12:03:16 -0300

    Tela informações turma

--- Source/API/app/controller/aulaController.js
+++ Source/API/app/controller/aulaController.js
@@ -1,57 +1,58 @@
-const AulaCRUD = require('../db/aulaCRUD');
+const AulaCRUD = require("../db/aulaCRUD");
 
 class AulaController {
-
     getByLinkId = async (req, res) => {
         const aulaCRUD = new AulaCRUD();
         const { linkId } = req.params;
-    
-        await aulaCRUD.getByLinkId(linkId)
-        .then((aula) => {
-            if (aula) {
-            res.status(200).json(aula);
-            } else {
-            res.status(404).json({ message: "Aula not found" });
-            }
-        })
-        .catch((error) => {
-        console.log(error);
-        res.status(500).json({ error: "Internal server error" });
-      });
 
-    }
+        await aulaCRUD
+            .getByLinkId(linkId)
+            .then((aula) => {
+                if (aula) {
+                    res.status(200).json(aula);
+                } else {
+                    res.status(404).json({ message: "Aula not found" });
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+                res.status(500).json({ error: "Internal server error" });
+            });
+    };
 
-  create = async (req, res) => {
-    const aulaCRUD = new AulaCRUD();
-    const { date, content, classSubjectTeacherId } = req.body;
+    create = async (req, res) => {
+        const aulaCRUD = new AulaCRUD();
+        const { date, content, classSubjectTeacherId } = req.body;
 
-    await aulaCRUD.create(date, content, classSubjectTeacherId)
-      .then((id) => {
-        res.status(201).json({ id });
-      })
-      .catch((error) => {
-        console.log(error);
-        res.status(500).json({ error: "Internal server error" });
-      });
-  }
+        await aulaCRUD
+            .create(date, content, classSubjectTeacherId)
+            .then((id) => {
+                res.status(201).json({ id });
+            })
+            .catch((error) => {
+                console.log(error);
+                res.status(500).json({ error: "Internal server error" });
+            });
+    };
 
-  delete = async (req, res) => {
-    const aulaCRUD = new AulaCRUD();
-    const { id } = req.body;
+    delete = async (req, res) => {
+        const aulaCRUD = new AulaCRUD();
+        const { id } = req.body;
 
-    await aulaCRUD.delete(id)
-      .then(() => {
-        res.status(200).json({ message: "Aula deleted successfully" });
-      })
-      .catch((error) => {
-        if (error.name === "Not found") {
-          res.status(404).json({ message: "Aula not found" });
-        } else {
-          console.log(error);
-          res.status(500).json({ error: "Internal server error" });
-        }
-      });
-  }
+        await aulaCRUD
+            .delete(id)
+            .then(() => {
+                res.status(200).json({ message: "Aula deleted successfully" });
+            })
+            .catch((error) => {
+                if (error.name === "Not found") {
+                    res.status(404).json({ message: "Aula not found" });
+                } else {
+                    console.log(error);
+                    res.status(500).json({ error: "Internal server error" });
+                }
+            });
+    };
 }
 
 module.exports = AulaController;
